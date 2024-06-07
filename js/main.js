372499fd60c0a309177ad9fa43bb3b2c8762012d
pokkyy <arqwbu@gmail.com>
2024-06-07 20:00:38 +0800

    map base

--- js/main.js
+++ js/main.js
@@ -17,34 +17,78 @@ d3.csv("data/ds_salaries.csv", d3.autoType).then(function(data){
     // Load GeoJSON data of world countries
     // ("admin", "iso_a2", "geometry")
     d3.json("/data/custom.geo.json").then(function(geojson) {
-        // filter invalid
-        geojson.features = data.features.filter(function(feature) {
-            return feature.properties.name !== "Antarctica" && feature.geometry;
+        // get salary
+        var salaryDict = {};
+        data.forEach(function(d) {
+            if (!salaryDict[d.company_location]) {
+                salaryDict[d.company_location] = {total: 0, count: 0};
+            }
+            salaryDict[d.company_location].total += +d.salary_in_usd;
+            salaryDict[d.company_location].count += 1;
         });
-    
+
+        // Calculate average salary for each location
+        var averageSalaryDict = {};
+        for (var location in salaryDict) {
+            averageSalaryDict[location] = salaryDict[location].total / salaryDict[location].count;
+        }
+        console.log(averageSalaryDict);
+
+        // var colorScale = d3.scaleThreshold()
+        //     .domain([25000, 50000, 75000, 100000, 150000, 200000, 250000]) // Define threshold values
+        //     .range(['#DBE7C9', '#B3CC8F', '#789461', '#345830', '#1A1F16']); // Define corresponding colors
+        var colorScale = d3.scaleLinear()
+            .domain([d3.min(Object.values(averageSalaryDict)),
+                    d3.max(Object.values(averageSalaryDict))])
+            .range(['#B3CC8F', '#1A1F16']);   
+
+            // Filter out Antarctica
+        var filteredGeojson = geojson.features.filter(function(feature) {
+            return feature.properties.name !== "Antarctica";
+        });
+
         // Add GeoJSON layer to map
-        L.geoJSON(geojson, {
+        L.geoJSON(filteredGeojson, {
             style: function(feature) {
+                var averageSalary = averageSalaryDict[feature.properties.iso_a2] || 0;
                 return {
-                    fillColor: getColor(feature.properties.counts),
+                    fillColor: colorScale(averageSalary),
                     weight: 1,
                     opacity: 1,
                     color: 'white',
-                    fillOpacity: 0.7
+                    fillOpacity: 0.8
                 };
             }
         }).addTo(map);
+
+        // Add legend to map
+        var legend = L.control({ position: 'bottomright' });
+
+        legend.onAdd = function (map) {
+            var div = L.DomUtil.create('div', 'info legend'),
+                grades = [0, 50000, 100000, 150000, 200000, 250000],
+                labels = [],
+                from, to;
+        
+            // Add legend title
+            div.innerHTML += '<strong>Average Salary</strong><br>';
+        
+            // Loop through the grades to generate labels with colored squares
+            for (var i = 0; i < grades.length; i++) {
+                from = grades[i];
+                to = grades[i + 1];
+        
+                labels.push(
+                    '<i style="background:' + colorScale(from + 1) + '"></i> ' +
+                    from + (to ? '&ndash;' + to : '+'));
+            }
+        
+            div.innerHTML += labels.join('<br>');
+            return div;
+        };
+        
+        legend.addTo(map);
     });
-
-    // Define color scale for choropleth, ffix later
-    function getColor(d) {
-        return d > 250000 ? '#1A1F16' :
-            d > 200000 ? '#AE3F20' :
-            d > 100000 ? '#345830' :
-            d > 50000 ? '#4A7856' :
-            '#94ECBE';
-    }
-
     // Graphs
     // Job table
     // Job numbers
